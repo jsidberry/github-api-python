6c60577aac1031a7101dec1b51a98a0f6e8d9a3d
Juan Sidberry <jsidberry@gmail.com>
2021-10-15 19:30:21 -0400

    added env_vars

--- github_api.py
+++ github_api.py
@@ -1,13 +1,14 @@
 from github import Github
+import os
 
 # First create a Github instance:
-
+access_token = os.environ["GITHUB_API_ACCESS_TOKEN"]
 # using an access token
-g = Github("access_token")
+g = Github(access_token)
 
 # Github Enterprise with custom hostname
-g = Github(base_url="https://{hostname}/api/v3", login_or_token="access_token")
+g = Github(base_url="https://github.com/api/v3", login_or_token=access_token)
 
 # Then play with your Github objects:
 for repo in g.get_user().get_repos():
-    print(repo.name)
+    print(repo)
